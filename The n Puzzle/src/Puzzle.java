ca548766103a44adf1f713a5059695f347325621
FahmidAR <1705087@ugrad.cse.buet.ac.bd>
2021-11-30 02:08:58 +0600

    N puzzle Ai Final

--- The n Puzzle/src/Puzzle.java
+++ The n Puzzle/src/Puzzle.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
+
+import static java.lang.StrictMath.abs;
 
 public class Puzzle {
 
@@ -17,7 +22,7 @@ public class Puzzle {
         int k = 1, n;
 
         while (true) {
-            System.out.println("Enter Your value of K = ");
+            System.out.print("Enter Your value of K = ");
             k = userInput.nextInt();
 
             n = k * k - 1;
@@ -43,8 +48,7 @@ public class Puzzle {
             String[] row;
             String line = userInput2.nextLine();
 
-            if (line == null)
-            {
+            if (line == null) {
                 System.out.println("Enter a valid State!");
                 i--;
                 continue;
@@ -52,14 +56,10 @@ public class Puzzle {
 
             row = line.split(" ");
 
-            for (int ii = 0; ii < k; ii++)
-            {
-                if(row[ii].equals("*"))
-                {
+            for (int ii = 0; ii < k; ii++) {
+                if (row[ii].equals("*")) {
                     initialState.state[i][ii] = -1;
-                }
-                else
-                {
+                } else {
                     initialState.state[i][ii] = Integer.parseInt(row[ii]);
                 }
             }
@@ -71,16 +71,13 @@ public class Puzzle {
         State goalState = new State(k);
         int count = 1;
 
-        for (int i = 0; i < k; i++)
-        {
-            for (int ii = 0; ii < k; ii++)
-            {
+        for (int i = 0; i < k; i++) {
+            for (int ii = 0; ii < k; ii++) {
                 goalState.state[i][ii] = count;
                 count++;
 
-                if(count == k*k)
-                {
-                    count= -1;
+                if (count == k * k) {
+                    count = -1;
                 }
             }
         }
@@ -88,17 +85,265 @@ public class Puzzle {
         System.out.println("\n The Goal state is = \n");
         goalState.PrintBoard();
 
-        if(initialState.Solvable())
-        {
+        if (initialState.Solvable()) {
             System.out.println("\n## The Puzzle is Solvable ##\n");
-        }
-        else
-        {
+
+            System.out.println("## Using A * with Hamming Heauristic ##");
+            aStarAlgo(initialState,goalState,new Context(new Hamming()));
+            System.out.println("\n## Using A * with Hamming Manhattan ##");
+            aStarAlgo(initialState,goalState,new Context(new Manhattan()));
+            System.out.println("\n## Using A * with Hamming LinearConflict ##");
+            aStarAlgo(initialState,goalState,new Context(new LinearConflict()));
+
+        } else {
             System.out.println("\n## The Puzzle is Un-Solvable ##\n");
         }
 
-        
+    }
 
+
+    public interface Strategy {
+        public int doOperation(State current , State goal, int row[] , int col[]);
+    }
+
+    public static class Hamming implements Strategy{
+        @Override
+        public int doOperation(State current , State goal, int row[] , int col[]){
+            int hammingConflict = 0 ;
+            for (int i = 0; i < current.number; i++) {
+                for (int ii = 0; ii < current.number; ii++) {
+
+                    if(current.state[i][ii]!=-1&&current.state[i][ii]!=goal.state[i][ii])
+                    {
+                        hammingConflict++;
+                    }
+                }
+            }
+            return hammingConflict;
+        }
+    }
+
+    public static class Manhattan implements Strategy{
+        @Override
+        public int doOperation(State current , State goal, int row[] , int col[]){
+            int ManhattanConflict = 0 ;
+
+            for (int i = 0; i < current.number; i++) {
+                for (int ii = 0; ii < current.number; ii++) {
+
+                    if(current.state[i][ii]!=-1&&current.state[i][ii]!=goal.state[i][ii])
+                    {
+                        ManhattanConflict+=abs(row[current.state[i][ii]]-i)+abs(col[current.state[i][ii]]-ii);
+                    }
+                }
+            }
+
+
+
+            return ManhattanConflict;
+        }
+    }
+
+    public static class LinearConflict implements Strategy{
+        @Override
+        public int doOperation(State current , State goal, int row[] , int col[]){
+            int LinearConflictConflict = 0 ;
+
+            for (int i = 0; i < current.number; i++) {
+                for (int ii = 0; ii < current.number; ii++) {
+
+                    if(current.state[i][ii]!=-1&&current.state[i][ii]!=goal.state[i][ii])
+                    {
+                        LinearConflictConflict+=abs(row[current.state[i][ii]]-i)+abs(col[current.state[i][ii]]-ii);
+                    }
+                }
+            }
+
+            for (int i = 0; i < current.number; i++) {
+                for (int ii = 0; ii < current.number; ii++) {
+                    for (int iii = ii+1; iii < current.number; iii++) {
+
+                        if (current.state[i][ii] != -1 && current.state[i][iii]!=-1) {
+                            if(row[current.state[i][ii]]==i&&row[current.state[i][ii]]==row[current.state[i][iii]])
+                                if(current.state[i][ii]>current.state[i][iii])
+                                {
+                                    LinearConflictConflict += 2;
+                                }
+                        }
+                    }
+                }
+            }
+
+            return LinearConflictConflict;
+        }
+    }
+
+    public static class Context {
+        private Strategy strategy;
+
+        public Context(Strategy strategy){
+            this.strategy = strategy;
+        }
+
+        public int executeStrategy(State current , State goal, int row[] , int col[]){
+            return strategy.doOperation(current, goal, row , col);
+        }
+    }
+
+    public static void aStarAlgo(State initial , State goal , Context context)
+    {
+
+        int goalStateRow[] = new int[goal.number*goal.number];
+        int goalStateCol[] = new int[goal.number*goal.number];
+
+        for (int i = 0; i < goal.number; i++) {
+            for (int ii = 0; ii < goal.number; ii++) {
+
+                if(goal.state[i][ii]!=-1)
+                {
+                    goalStateRow[goal.state[i][ii]] = i;
+                    goalStateCol[goal.state[i][ii]] = ii;
+                }
+            }
+        }
+
+
+
+        PriorityQueue<State> openQueue = new PriorityQueue<State>(50, new Comparator<State>() {
+            @Override
+            public int compare(State o1, State o2) {
+                if(o1.heuristic +o1.level> o2.heuristic +o2.level) {
+                    return 1;
+                } else if (o1.heuristic +o1.level< o2.heuristic +o2.level) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+
+        });
+        ArrayList<State> closeList = new ArrayList<>();
+
+
+        initial.heuristic = context.executeStrategy(initial,goal,goalStateCol,goalStateCol);
+        openQueue.add(initial);
+
+        int optCost = 0 ;
+
+        while (!openQueue.isEmpty())
+        {
+            State now = openQueue.peek();
+
+            //now.PrintBoard();
+
+            if (now.heuristic == 0) {
+                optCost= now.level;
+                //now.PrintBoard();
+                now.PrintParrentTrack();
+                break;
+            }
+
+            closeList.add(now);
+            openQueue.poll();
+
+            State u = new State(initial.number);
+            State d = new State(initial.number);
+            State l = new State(initial.number);
+            State r = new State(initial.number);
+
+            u=stateCopy(now,u);
+            d=stateCopy(now,d);
+            l=stateCopy(now,l);
+            r=stateCopy(now,r);
+
+            u = u.upChild();
+            if(u!=null && arrayContain(closeList,u)!=true) {
+                //u.PrintBoard();
+                u.heuristic = context.executeStrategy(u,goal,goalStateRow,goalStateCol);
+                //System.out.println(u.heuristic);
+                u.level = now.level+1;
+                u.parent = now;
+                openQueue.add(u);
+            }
+
+            d = d.downChild();
+            if(d!=null && arrayContain(closeList,d)!=true) {
+                //d.PrintBoard();
+                d.heuristic = context.executeStrategy(d, goal,goalStateRow,goalStateCol);
+                //System.out.println(d.heuristic);
+                d.level = now.level+1;
+                d.parent = now;
+                openQueue.add(d);
+            }
+
+            l = l.leftChild();
+            if(l!=null && arrayContain(closeList,l)!=true) {
+                //l.PrintBoard();
+                l.heuristic = context.executeStrategy(l,goal,goalStateRow,goalStateCol);
+                //System.out.println(l.heuristic);
+                l.level = now.level+1;
+                l.parent = now;
+                openQueue.add(l);
+            }
+
+            r = r.rightChild();
+            if(r!=null && arrayContain(closeList,r)!=true) {
+                //r.PrintBoard();
+                r.heuristic = context.executeStrategy(r,goal,goalStateRow,goalStateCol);
+               // System.out.println(r.heuristic);
+                r.level = now.level+1;
+                r.parent = now;
+                openQueue.add(r);
+            }
+
+
+        }
+
+        System.out.println("Optimal Cost = "+ optCost);
+        System.out.println("Expanded Node = "+ closeList.size());
+        int nodeCount = closeList.size() + openQueue.size();
+        System.out.println("Explored Node = "+nodeCount);
+
+    }
+
+    public static boolean arrayContain(ArrayList<State> AL ,State A)
+    {
+        for (int i = 0 ; i< AL.size();i++)
+        {
+            if(arrayCompare(AL.get(i).state,A.state))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean arrayCompare(int a[][] , int b[][])
+    {
+        boolean ret = true ;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int ii = 0; ii < a.length; ii++) {
+
+                if(a[i][ii]!=b[i][ii]) ret = false;
+            }
+        }
+
+        return ret;
+    }
+
+    public static State stateCopy(State A ,State B)
+    {
+        int n = A.number;
+        B.number = n ;
+
+        for (int i = 0; i < n; i++) {
+            for (int ii = 0; ii < n; ii++) {
+                B.state[i][ii]=A.state[i][ii];
+            }
+        }
+
+        return B;
     }
 
 }
--- The n Puzzle/src/State.java
+++ The n Puzzle/src/State.java
@@ -1,8 +1,14 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class State {
 
     int number ;
     int[][] state ;
     State parent = null;
+    int level = 0 ;
+    int heuristic ;
 
     public State(int theNumber) {
         this.number = theNumber;
@@ -10,8 +16,160 @@ public class State {
     }
 
     public State(State another) {
+
         this.state = another.state;
         this.number = another.number;
+
+    }
+
+    public State upChild()
+    {
+        State temp = new State(this);
+
+        int indexRow = 0 , indexCol = 0;
+
+        for (int i = 0; i < this.number; i++) {
+            for (int ii = 0; ii < this.number; ii++) {
+
+                if (this.state[i][ii] == -1) {
+
+                    indexRow = i ;
+                    indexCol = ii;
+                    break;
+                }
+            }
+        }
+
+        if(indexRow==0)
+        {
+            return null;
+        }
+        else
+        {
+            temp.state[indexRow][indexCol] = this.state[indexRow-1][indexCol];
+            temp.state[indexRow-1][indexCol] = -1;
+            return temp;
+        }
+
+
+    }
+
+    public int Hamming()
+    {
+        int realValue = 1;
+        int HammingValue = 0;
+
+        for (int i = 0; i < this.number; i++) {
+            for (int ii = 0; ii < this.number; ii++) {
+
+                if(this.state[i][ii]!=-1)
+                {
+                    if(this.state[i][ii]!= realValue)
+                    {
+                        HammingValue++;
+                    }
+                    realValue++;
+                }
+            }
+        }
+
+        return HammingValue;
+    }
+
+    public State downChild()
+    {
+        State temp = new State(this);
+
+        int indexRow = 0 , indexCol = 0;
+
+        for (int i = 0; i < this.number; i++) {
+            for (int ii = 0; ii < this.number; ii++) {
+
+                if (this.state[i][ii] == -1) {
+
+                    indexRow = i ;
+                    indexCol = ii;
+                    break;
+                }
+            }
+        }
+
+        if(indexRow==this.number-1)
+        {
+            return null;
+        }
+        else
+        {
+            temp.state[indexRow][indexCol] = this.state[indexRow+1][indexCol];
+            temp.state[indexRow+1][indexCol] = -1;
+            return temp;
+        }
+
+
+    }
+
+    public State leftChild()
+    {
+        State temp = new State(this);
+
+        int indexRow = 0 , indexCol = 0;
+
+        for (int i = 0; i < this.number; i++) {
+            for (int ii = 0; ii < this.number; ii++) {
+
+                if (this.state[i][ii] == -1) {
+
+                    indexRow = i ;
+                    indexCol = ii;
+                    break;
+                }
+            }
+        }
+
+        if(indexCol==0)
+        {
+            return null;
+        }
+        else
+        {
+            temp.state[indexRow][indexCol] = this.state[indexRow][indexCol-1];
+            temp.state[indexRow][indexCol-1] = -1;
+            return temp;
+        }
+
+
+    }
+
+    public State rightChild()
+    {
+        State temp = new State(this);
+
+        int indexRow = 0 , indexCol = 0;
+
+        for (int i = 0; i < this.number; i++) {
+            for (int ii = 0; ii < this.number; ii++) {
+
+                if (this.state[i][ii] == -1) {
+
+                    indexRow = i ;
+                    indexCol = ii;
+                    break;
+                }
+            }
+        }
+
+        if(indexCol==this.number-1)
+        {
+            return null;
+        }
+        else
+        {
+            temp.state[indexRow][indexCol] = this.state[indexRow][indexCol+1];
+            temp.state[indexRow][indexCol+1] = -1;
+            return temp;
+        }
+
+
     }
 
     Boolean Solvable()
@@ -105,6 +263,40 @@ public class State {
         }
     }
 
+    void PrintParrentTrack()
+    {
+        //PrintBoard();
+
+        State temp = parent ;
+
+        ArrayList<State> solve= new ArrayList<>();
+        solve.add(this);
+
+        while(temp!=null)
+        {
+            //temp.PrintBoard();
+            solve.add(temp);
+            temp = temp.parent;
+        }
+
+        Collections.reverse(solve);
+
+        for (int i = 0 ; i < solve.size();i++)
+        {
+            if(i!=0)
+            {
+                System.out.print("             ");
+                for (int ii = 1; ii < this.number-1; ii++)
+                {
+                    System.out.print("   ⟱  ⟱  ⟱   ");
+                }
+                System.out.print("            \n\n");
+
+            }
+            solve.get(i).PrintBoard();
+        }
+    }
+
 
     void PrintBoard() {
 
@@ -139,8 +331,10 @@ public class State {
             {
                 System.out.print("=============");
             }
-            System.out.print("=============\"\n");
+            System.out.println("=============\"");
         }
+        System.out.println();
+        System.out.println();
 
     }
 
